44e1be83c2dc87109cd0fec1d7793a78fa256a2f
odarlie <odarlie@gmail.com>
2017-06-12 18:19:51 +0300

    updated the tests for the function and included tests for non_primes and negatives

--- test_generate_prime_number.py
+++ test_generate_prime_number.py
@@ -1,14 +1,21 @@
 import unittest
-from calc_prime_numbers import gen_prime_numbers
+from calc_prime_numbers import prime
 
-class test_generate_prime_numbers(self):
+class test_generate_prime_numbers(unittest.TestCase):
     def test_prime_output(self):
-        self.assertEqual(gen_prime_numbers(20),[ 2, 3, 5, 7, 11, 13, 17, 19])
-    def test_input_string(self):
-        self.assertEqual(gen_prime_numbers('string'),"Please input a number")
-    def test_output_is_list(self):
-        self.assertEqual(gen_prime_numbers(20),list)
-##    def test_output_for_negative_input(self):
-##        self.assertEqual(gen_prime_numbers(-2),'No negative prime numbers')
-        
+        self.assertEqual(prime(6),[ 2, 3, 5])
+    def test_string (self):
+        self.assertEqual(prime('string'),"Invalid Input")
+    def test_output_list(self):
+        self.assertTrue(isinstance(prime(6),list))
+    def test_output_negatives(self):
+        self.assertEqual(prime(-2),'no negative prime numbers')
+    def test_output_non_primes(self):
+        self.assertNotIn([0,1,4,6],prime(6))
+    
+    
+    
+
+
+    
 unittest.main()
